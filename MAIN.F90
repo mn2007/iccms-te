9dd9c9f42370cbb48a75c5b06ede0e533f3267cd
noda <noda@ccs.tsukuba.ac.jp>
2018-10-03 13:11:50 +0900

    add main.F90

---
+++ MAIN.F90
@@ -0,0 +1,21 @@
+program main
+  implicit none
+  integer :: N          ! Size of Discretized System
+  real(8),allocatable :: V(:)     ! External Potential Field
+  real(8) :: L          ! Length of Computational Domain
+  real(8) :: E          ! Eigenenergy
+  real(8),allocatable :: PHI(:)  ! Wavefunction
+
+  !N=100
+  !L=10.d0
+
+  ! allocate(V(1:N))
+  ! allocate(PHI(1:N))
+
+  call GS(N, V, L, E, PHI)
+  call RT(N, V, L, E, PHI)
+
+  ! deallocate(V)
+  ! deallocate(PHI)
+
+end program
